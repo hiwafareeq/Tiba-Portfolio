b3d1e1b3a62106104c516bd726892c332628a494
aryfareq <121553546+aryfareq@users.noreply.github.com>
2026-01-03 02:41:53 +0300

    Imported Lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import './App.css'
 import Navbar from './components/navbar'
 import LandingPage from './pages/LandingPage'
 import AboutMe from './pages/AboutMe'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, [])
   return (
     <div>
       <Navbar />
