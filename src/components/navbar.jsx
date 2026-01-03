612b645ca71bebfd5eea9b429177087299b51218
aryfareq <121553546+aryfareq@users.noreply.github.com>
2026-01-03 03:27:59 +0300

    gradient added to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,22 @@
-import Logo from '../assets/images/TibaLogo.svg'
+import Logo from "../assets/images/TibaLogo.svg";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 m-4 sticky top-0 z-10">
+    <nav className="sticky top-0 z-20">
+      <div className="flex justify-between items-center p-8
+        bg-gradient-to-b from-black/80 to-black/0
+      ">
         <img src={Logo} alt="Logo" className="w-19 h-13" />
+
         <ul className="flex gap-7">
-            <li className="text-white text-lg">Home</li>
-            <li className="text-white text-lg">About</li>
-            <li className="text-white text-lg">Services</li>
-            <li className="text-white text-lg">Contact</li>
+          <li className="text-white text-lg cursor-pointer hover:opacity-80">Home</li>
+          <li className="text-white text-lg cursor-pointer hover:opacity-80">About</li>
+          <li className="text-white text-lg cursor-pointer hover:opacity-80">Services</li>
+          <li className="text-white text-lg cursor-pointer hover:opacity-80">Contact</li>
         </ul>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
